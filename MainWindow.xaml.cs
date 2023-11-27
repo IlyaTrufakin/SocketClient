88d4a0233fce18f129775dbd1f0a9d98c21a3b38
Ilya Trufakin <trufakin@hotmail.com>
2023-11-28 00:02:00 +0200

    Ver0.01

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,40 @@ namespace SocketClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static int port = 8005;
+        private static string IPAdress = "127.0.0.1";
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private static void SocketInit()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(IPAdress), port);
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            clientSocket.Connect(ipPoint);
+
+            Console.WriteLine("Enter data to send: ");
+            string message = Console.ReadLine();
+            byte[] sendData = Encoding.Unicode.GetBytes(message);
+            clientSocket.Send(sendData);
+
+            StringBuilder recievedString = new StringBuilder();
+            int recievedBytes = 0;
+            byte[] receiveData = new byte[4096];
+
+            do
+            {
+                recievedBytes = clientSocket.Receive(receiveData, receiveData.Length, 0);
+                recievedString.Append(Encoding.Unicode.GetString(receiveData, 0, recievedBytes));
+            } while (clientSocket.Available > 0);
+
+            Console.WriteLine("Server answer: " + recievedString.ToString());
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            Console.WriteLine("Socket close...");
         }
     }
 }
