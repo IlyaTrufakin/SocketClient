ab94610b0d66974f1c1d1a962d215f8e01ae4ad4
Ilya Trufakin <trufakin@hotmail.com>
2023-11-28 04:44:09 +0200

    Ver0.04

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,89 +18,23 @@ namespace SocketClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool ServerConnect = false;
-        private Socket? clientSocket;
+   
+        private ServerCommunication serverCommunication;
+ 
         public MainWindow()
         {
             InitializeComponent();
+            serverCommunication = new ServerCommunication();
         }
 
-        private static IPEndPoint SocketInit(string ipAddress, string port)
-        {
-            try
-            {
-                if (!int.TryParse(port, out int portNumber))
-                {
-                    throw new ArgumentException("Порт задан неверно (должен быть числом)");
-                }
-
-                return new IPEndPoint(IPAddress.Parse(ipAddress), portNumber);
-            }
-            catch (FormatException ex)
-            {
-                throw new FormatException("Некорректный формат IP-адреса", ex);
-            }
-            catch (ArgumentException ex)
-            {
-                throw new ArgumentException("ошибка аргумента: " + ex.Message, ex);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message, ex);
-            }
-        }
-
-        private bool StartClient(string ipAddress, string port)
-        {
-            try
-            {
-                IPEndPoint? endpoint = SocketInit(ipAddress, port);
-
-                if (endpoint != null)
-                {
-                    OutputWindow.Text += "IPEndPoint успешно создан.\n";
-                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                    try
-                    {
-                        clientSocket.Connect(endpoint);
-                        OutputWindow.Text += "Соединение с сервером установлено\n";
-                        ScrollTextBlock.ScrollToEnd();
-                        return true;
-                    }
-                    catch (SocketException ex)
-                    {
-                        // Обработка исключений сокета при установлении соединения
-                        OutputWindow.Text += "Ошибка соединения: " + ex.Message + Environment.NewLine;
-                    }
-
-                }
-                else
-                {
-                    OutputWindow.Text += "IPEndPoint не создан из-за ошибки в методе SocketInit.\n";
-                }
-            }
-
-            catch (Exception ex)
-            {
-                OutputWindow.Text += "Произошла ошибка: " + ex.Message + Environment.NewLine;
-            }
-            ScrollTextBlock.ScrollToEnd();
-            return false;
-
-        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ServerConnect = StartClient(ipAddress.Text, portNumber.Text);
-            if (ServerConnect)
-            {
-                SendMessageButton.IsEnabled = true;
-            }
-            else 
-            { SendMessageButton.IsEnabled = false; }
+            string connectionResult = serverCommunication.ConnectToServer(ipAddress.Text, portNumber.Text);
+            OutputWindow.Text += connectionResult + Environment.NewLine;
+            SendMessageButton.IsEnabled = serverCommunication.IsConnected();
         }
 
-  
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
@@ -109,28 +43,10 @@ namespace SocketClient
 
         private void SendMessageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (ServerConnect && clientSocket != null && clientSocket.Connected)
+            if (serverCommunication.IsConnected())
             {
-                byte[] sendData = Encoding.Unicode.GetBytes(InputWindow.Text);
-                clientSocket.Send(sendData);
-                OutputWindow.Text += "Отправлено сообщение: " + InputWindow.Text + Environment.NewLine;
-
-                byte[] receiveData = new byte[256];
-                StringBuilder receivedString = new StringBuilder();
-                int bytesReceived;
-
-                while (true)
-                {
-                    bytesReceived = clientSocket.Receive(receiveData);
-                    receivedString.Append(Encoding.Unicode.GetString(receiveData, 0, bytesReceived));
-
-                    if (bytesReceived < receiveData.Length)
-                    {
-                        break; // Выход из цикла, если больше данных не ожидается
-                    }
-                }
-
-                OutputWindow.Text += "Server answer: " + receivedString.ToString() + Environment.NewLine;
+                string response = serverCommunication.SendMessage(InputWindow.Text);
+                OutputWindow.Text += "Server answer: " + response + Environment.NewLine;
             }
             else
             {
---
+++ SocketClient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    public class ServerCommunication
+    {
+        private Socket? clientSocket;
+
+        public string ConnectToServer(string ipAddress, string port)
+        {
+            try
+            {
+                IPEndPoint endpoint = SocketInit(ipAddress, port);
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                clientSocket.Connect(endpoint);
+                return "Соединение с сервером установлено";
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка соединения с сервером: " + ex.Message;
+            }
+        }
+
+        public bool IsConnected()
+        {
+            return clientSocket != null && clientSocket.Connected;
+        }
+
+        public string SendMessage(string message)
+        {
+            try
+            {
+                byte[] sendData = Encoding.Unicode.GetBytes(message);
+                clientSocket.Send(sendData);
+
+                byte[] receiveData = new byte[256];
+                StringBuilder receivedString = new StringBuilder();
+                int bytesReceived;
+
+                while (true)
+                {
+                    bytesReceived = clientSocket.Receive(receiveData);
+                    receivedString.Append(Encoding.Unicode.GetString(receiveData, 0, bytesReceived));
+
+                    if (bytesReceived < receiveData.Length)
+                    {
+                        break;
+                    }
+                }
+
+                return receivedString.ToString();
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка отправки сообщения/получения ответа: " + ex.Message;
+            }
+        }
+
+        private IPEndPoint SocketInit(string ipAddress, string port)
+        {
+            try
+            {
+                if (!int.TryParse(port, out int portNumber))
+                {
+                    throw new ArgumentException("Порт задан неверно (должен быть числом)");
+                }
+
+                return new IPEndPoint(IPAddress.Parse(ipAddress), portNumber);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка инициализации сокета: " + ex.Message);
+            }
+        }
+    }
+}
