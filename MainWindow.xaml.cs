982c11d357b503a253987db4af3787f790ed62f3
Ilya Trufakin <trufakin@hotmail.com>
2023-11-28 03:57:46 +0200

    Ver0.03

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,8 @@ namespace SocketClient
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        private bool ServerConnect = false;
+        private Socket? clientSocket;
         public MainWindow()
         {
             InitializeComponent();
@@ -49,7 +50,7 @@ namespace SocketClient
             }
         }
 
-        private void StartClient(string ipAddress, string port)
+        private bool StartClient(string ipAddress, string port)
         {
             try
             {
@@ -58,6 +59,19 @@ namespace SocketClient
                 if (endpoint != null)
                 {
                     OutputWindow.Text += "IPEndPoint успешно создан.\n";
+                    clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    try
+                    {
+                        clientSocket.Connect(endpoint);
+                        OutputWindow.Text += "Соединение с сервером установлено\n";
+                        ScrollTextBlock.ScrollToEnd();
+                        return true;
+                    }
+                    catch (SocketException ex)
+                    {
+                        // Обработка исключений сокета при установлении соединения
+                        OutputWindow.Text += "Ошибка соединения: " + ex.Message + Environment.NewLine;
+                    }
 
                 }
                 else
@@ -71,13 +85,57 @@ namespace SocketClient
                 OutputWindow.Text += "Произошла ошибка: " + ex.Message + Environment.NewLine;
             }
             ScrollTextBlock.ScrollToEnd();
-
+            return false;
 
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            StartClient(ipAddress.Text, portNumber.Text);
+            ServerConnect = StartClient(ipAddress.Text, portNumber.Text);
+            if (ServerConnect)
+            {
+                SendMessageButton.IsEnabled = true;
+            }
+            else 
+            { SendMessageButton.IsEnabled = false; }
+        }
+
+  
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SendMessageButton.IsEnabled = false;
+        }
+
+        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ServerConnect && clientSocket != null && clientSocket.Connected)
+            {
+                byte[] sendData = Encoding.Unicode.GetBytes(InputWindow.Text);
+                clientSocket.Send(sendData);
+                OutputWindow.Text += "Отправлено сообщение: " + InputWindow.Text + Environment.NewLine;
+
+                byte[] receiveData = new byte[256];
+                StringBuilder receivedString = new StringBuilder();
+                int bytesReceived;
+
+                while (true)
+                {
+                    bytesReceived = clientSocket.Receive(receiveData);
+                    receivedString.Append(Encoding.Unicode.GetString(receiveData, 0, bytesReceived));
+
+                    if (bytesReceived < receiveData.Length)
+                    {
+                        break; // Выход из цикла, если больше данных не ожидается
+                    }
+                }
+
+                OutputWindow.Text += "Server answer: " + receivedString.ToString() + Environment.NewLine;
+            }
+            else
+            {
+                OutputWindow.Text += "Сообщение не отправлено: нет соединения с сервером" + Environment.NewLine;
+            }
         }
     }
 }
